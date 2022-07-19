9047d69bcd0c6dcc2f0c3a25d17b7f5d882cb25b
khalidshabbir089 <fa19-bse-015@students.cuisahiwal.edu.pk>
2022-07-19 21:56:28 +0500

    first commit

---
+++ src/baseurl/baseurl.js
@@ -0,0 +1,3 @@
+export const baseurl = "https://cuitimetableserver.herokuapp.com";
+// export const baseurl = "https://cuitimetable.herokuapp.com/";
+// "proxy": "https://cuitimetable.herokuapp.com/",
